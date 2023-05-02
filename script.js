27645bcbe1f00fc24b8a14873e12f4f27fce576e
Aleandro_Resendiz <ale_resendiz321@hotmail.com>
2023-05-02 00:01:09 -0600

    se tuvo que arreglar errores respecto al diseño resposivo y que algunos elementos se generaro en js

--- script.js
+++ script.js
@@ -2,14 +2,21 @@
 function encriptar(){
     var texto = document.getElementById("msn").value;
     var tituloM = document.createElement("h2");
+    tituloM.classList.add("js-output");
+
     var salida = document.getElementById("salida");
     var muñeco = document.getElementById("muñeco");
+    muñeco.classList.add("js-muñeco");
     const contenedor = document.getElementById("output");
+
     const h1 = document.createElement("h1");
+    h1.classList.add("js-h1");
+
     const btn_copy = document.createElement("button");
+    btn_copy.classList.add("js-btnCopy");
+
     let desencriptado = false;
 
-    tituloM.style.fontSize = '24px';
     tituloM.style.marginTop = '20px';
     tituloM.style.background = "#fff";
 
@@ -26,7 +33,32 @@ function encriptar(){
         tituloM.textContent = "Texto encriptado con éxito";
         document.getElementById("msn").value = "";
         h1.appendChild(txt1);
-        h1.style.fontSize = '32px';
+        
+        if (window.innerWidth <= 768){
+            // contenedor.style.outline = '1px solid red';
+            contenedor.style.background = 'none';
+            contenedor.style.height = '60%';
+
+            tituloM.style.fontSize = '14px';
+            h1.style.fontSize = '22px';
+            tituloM.style.background = "none";
+
+            btn_copy.style.width = "80%"
+            btn_copy.style.marginLeft = '10%'
+            btn_copy.style.marginBottom = '40%';
+        }
+        else if(window.innerWidth <= 357){
+            tituloM.style.fontSize = '14px';
+            h1.style.fontSize = '22px';
+            tituloM.style.background = "none";
+            btn_copy.style.width = "80%"
+            btn_copy.style.marginLeft = '10%'
+        }else{
+            tituloM.style.fontSize = '24px';
+            h1.style.fontSize = '32px';
+        }
+        
+        
         h1.style.marginTop = '100%';
         
         //Eliminar los hijos antiguos de salida
@@ -34,21 +66,13 @@ function encriptar(){
             contenedor.removeChild(contenedor.firstChild);
             
         }
-        // contenedor.style.outline = "1px solid red";
+
         contenedor.style.width = '336px';
         contenedor.style.height = 'auto';
         //Agregar ek boton copiar
         
         btn_copy.textContent = "Copiar";
-        btn_copy.style.borderRadius = '24px';
-        btn_copy.style.border = '1px solid #0A3871'
-        btn_copy.style.fontWeight = '400';
-        btn_copy.style.width = '336px';
-        btn_copy.style.height = '67px';
-        btn_copy.style.fontSize = '19px';
-        btn_copy.style.marginTop = '50%';
-        
-        
+
         btn_copy.addEventListener("click",function(){
             let range = document.createRange();
             range.selectNodeContents(h1);
@@ -71,7 +95,15 @@ function encriptar(){
         desencriptado = true;
     }else{
         if(!desencriptado){
-            muñeco.style.display = 'block';
+            if(window.innerWidth <= 768){
+                muñeco.style.display = 'none';
+            }
+            else if(window.innerWidth <= 375){
+                muñeco.style.display = 'none';
+            }else{
+                tituloM.style.fontSize = '24px';
+                h1.style.fontSize = '32px';
+            }
         }
         tituloM.textContent = "Ningún mensaje fue encontrado";
         contenedor.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
@@ -82,50 +114,78 @@ function encriptar(){
 function desencriptar(){
     var texto = document.getElementById("msn").value;
     var tituloM = document.createElement("h2");
+    tituloM.classList.add("js-output");
+
     var salida = document.getElementById("salida");
     var muñeco = document.getElementById("muñeco");
+    muñeco.classList.add("js-muñeco");
+
     const contenedor = document.getElementById("output");
+
     const h1 = document.createElement("h1");
+    h1.classList.add("js-h1");
+
     const btn_copy = document.createElement("button");
+    btn_copy.classList.add("js-btnCopy");
+
     let desencriptado = false;
 
-    tituloM.style.fontSize = '24px';
+    
     tituloM.style.marginTop = '20px';
-    tituloM.style.background = "#fff";
+    
 
-    let textoCifrado = texto
+    let txtCifrado = texto
     .replace(/enter/gi, "e")
     .replace(/imes/gi, "i")
     .replace(/ai/gi, "a")
     .replace(/ober/gi, "o")
     .replace(/ufat/gi, "u");
 
-    if(texto.length !=0){
-        const txt1 = document.createTextNode(textoCifrado);
+    if(texto.length != 0){
+        const txt1 = document.createTextNode(txtCifrado);
 
-        tituloM.textContent = "Texto encriptado con exito";
+        tituloM.textContent = "Texto encriptado con éxito";
         document.getElementById("msn").value = "";
         h1.appendChild(txt1);
-        h1.style.fontSize = '32px';
-        h1.style.marginTop = '100%';
+        
+        if (window.innerWidth <= 768){
+            // contenedor.style.outline = '1px solid red';
+            contenedor.style.background = 'none';
+            contenedor.style.height = '60%';
 
-        // Eliminar los hijos antiguos de salida, si los hay
-        while (contenedor.firstChild) {
-            contenedor.removeChild(contenedor.firstChild);
+            tituloM.style.fontSize = '14px';
+            h1.style.fontSize = '22px';
+            tituloM.style.background = "none";
+
+            btn_copy.style.width = "80%"
+            btn_copy.style.marginLeft = '10%'
+            btn_copy.style.marginBottom = '40%';
         }
-        // estilo de contenedor
+        else if(window.innerWidth <= 357){
+            tituloM.style.fontSize = '14px';
+            h1.style.fontSize = '22px';
+            tituloM.style.background = "none";
+            btn_copy.style.width = "80%"
+            btn_copy.style.marginLeft = '10%'
+        }else{
+            tituloM.style.fontSize = '24px';
+            h1.style.fontSize = '32px';
+        }
+        
+        
+        h1.style.marginTop = '100%';
+        
+        //Eliminar los hijos antiguos de salida
+        while(contenedor.firstChild){
+            contenedor.removeChild(contenedor.firstChild);
+            
+        }
+
         contenedor.style.width = '336px';
         contenedor.style.height = 'auto';
-
+        
         //Agregar ek boton copiar
         btn_copy.textContent = "Copiar";
-        btn_copy.style.borderRadius = '24px';
-        btn_copy.style.border = '1px solid #0A3871'
-        btn_copy.style.fontWeight = '400';
-        btn_copy.style.width = '336px';
-        btn_copy.style.height = '67px';
-        btn_copy.style.fontSize = '19px';
-        btn_copy.style.marginTop = '50%';
 
         btn_copy.addEventListener("click",function(){
             let range = document.createRange();
@@ -137,18 +197,28 @@ function desencriptar(){
             swal("Copiado!", "El texto se ha copiado correctamente", "success");
         });
 
-        //agregar los ellementos a los contenedores
+        // Agregar elementos al contenedor
+        
         contenedor.appendChild(h1);
         contenedor.appendChild(tituloM);
         contenedor.appendChild(btn_copy);
 
+        // agregar contenido al elemento
+        
         muñeco.style.display = 'none';
         desencriptado = true;
-    } else {
-        if (!desencriptado) {
-            muñeco.style.display = 'block';
+    }else{
+        if(!desencriptado){
+            if(window.innerWidth <= 768){
+                muñeco.style.display = 'none';
+            }
+            else if(window.innerWidth <= 375){
+                muñeco.style.display = 'none';
+            }else{
+                tituloM.style.fontSize = '24px';
+                h1.style.fontSize = '32px';
+            }
         }
-
         tituloM.textContent = "Ningún mensaje fue encontrado";
         contenedor.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
         swal("Ooops!", "Debes ingresar un texto", "warning");
